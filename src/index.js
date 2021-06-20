16655caf5c233f3c7f48646d45253271e4089fc1
nikola116 <nikola116088@hotmail.com>
2021-06-20 22:34:29 +0800

    upload changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import TagManager from 'react-gtm-module'
+ 
+const tagManagerArgs = {
+    gtmId: 'GTM-5WM2H4D'
+}
+ 
+TagManager.initialize(tagManagerArgs)
 ReactDOM.render(
   <React.StrictMode>
     <App />
